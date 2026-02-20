ec76f6d9072a085b9b62a3bdd9db29135e413e07
Kavyashreent <Kavyashreent11@gmail.com>
2026-02-20 17:54:14 +0530

    Create package.js

---
+++ application/package.js
@@ -0,0 +1,18 @@
+{
+  "name": "appointment-service",
+  "version": "1.0.0",
+  "description": "Appointment Service API for managing patient appointments",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
